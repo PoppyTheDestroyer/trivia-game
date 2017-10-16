c80b5a9f766ce7e24434b666fbe673332a50e3e6
Jason Saenz <jasonsaenz@jasons-mbp.attlocal.net>
2017-10-15 20:14:38 -0500

    basic

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+//Object containing questions/answers, as well as differentiating correct answer
+
+//script to randomly choose question
+
+//display question and answer choices (choices as buttons)
+
+//display timer
+
+//screen showing user selected correct answer/displaying 
+//their correct answer in green - timed
+
+//screen showing user selected incorrect answer/displaying 
+//correct answer in red - timed
+
+//define reset function
+
+//show results page with number of questions right/wrong
+//restart button (calls reset function)
+
+//create time-out function that hides question/answers
+
+//
