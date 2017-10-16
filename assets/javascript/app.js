a7e19329d981f307bc98e8eddacf34cd1c28f7a5
Jason Saenz <jasonsaenz@jasons-mbp.attlocal.net>
2017-10-15 20:50:37 -0500

    questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,19 @@
 //Object containing questions/answers, as well as differentiating correct answer
-
-//script to randomly choose question
+var whoA = {
+    quest: "What is the title of the first episode featuring the 10th Doctor?",
+    ansA: "Plastic Christmas",
+    ansB: "Rose",
+    ansC: "Elastic Christmas",
+    ansD: "Violet",
+}
+var whoB = {
+    quest: "If you were to come face to face with a Dalek, what you most likely hear it say?",
+    ansA: "Eradicate",
+    
+}
+//Array of object names
+var questions = []
+//script to randomly choose question from above array
 
 //display question and answer choices (choices as buttons)
 
@@ -11,6 +24,8 @@
 
 //screen showing user selected incorrect answer/displaying 
 //correct answer in red - timed
+
+//add various related images to answer screen - this will look so dope.
 
 //define reset function
 
