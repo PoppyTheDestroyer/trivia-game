4a798fd8938940163f6141ca3acabc92da21c8a6
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-10-17 18:49:45 -0500

    questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,17 +2,71 @@
 var whoA = {
     quest: "What is the title of the first episode featuring the 10th Doctor?",
     ansA: "Plastic Christmas",
-    ansB: "Rose",
-    ansC: "Elastic Christmas",
-    ansD: "Violet",
+    ansB: "Elastic Christmas",
+    ansC: "Violet",
+    cor: "Rose"    
 }
 var whoB = {
     quest: "If you were to come face to face with a Dalek, what you most likely hear it say?",
     ansA: "Eradicate",
-    
+    ansB: "Eliminate",
+    ansC: "Evaluate",
+    cor: "Exterminate"    
+}
+var whoC = {
+    quest: "In the episode A Good Man Goes to War, the Doctor gives Col. Manton a new name. What was the new name?",
+    ansA: "Col. Surrender",
+    ansB: "Col. Bravery",
+    ansC: "Col. Sadface",
+    cor: "Col. Runaway"    
+}
+var whoD = {
+    quest: "Which of the following is NOT one of the names used by the character played by Maisie Williams?",
+    ansA: "Me",
+    ansB: "Ashildr",
+    ansC: "The Knightmare",
+    cor: "Arya",    
+}
+var whoE = {
+    quest: "How long did Rory stand guard over the Pandorica while Amy was trapped inside?",
+    ansA: "2 years",
+    ansB: "20 years",
+    ansC: "200 years",
+    cor: "2,000 years"
+}
+var whoF = {
+    quest: "By what name is Melody Pond best known?",
+    ansA: "Clara Oswald",
+    ansB: "Rose Tyler",
+    ansC: "Martha Jones",
+    cor: "River Song"    
+}
+var whoG = {
+    quest: "What famous artist did Amy and the Doctor go back in time to help?",
+    ansA: "Jackson Pollock",
+    ansB: "Leonardo da Vinci",
+    ansC: "Michaelangelo",
+    cor: "Vincent van Gogh"
+}
+var whoH = {
+    quest: "What performer played the War Doctor?",
+    ansA: "Simon Pegg",
+    ansB: "Emma Thompson",
+    ansC: "Emma Watson:",
+    cor: "John Hurt"
+}
+var whoI = {
+    quest: "What does TARDIS stand for?",
+    ansA: "Temporaral Aerial Redistributing Device Integration System",
+    ansB: "Technical Array Randomizer Deployment in Science",
+    ansC: "Terminal Alloy Restrictive Development Inverting Scenario",
+    cor: "Time and Relative Dimension in Space"
+}
+var whoJ = {
+    quest: ""
 }
 //Array of object names
-var questions = []
+var questions = [whoA, whoB, whoC, whoD, whoE, whoF, whoG]
 //script to randomly choose question from above array
 
 //display question and answer choices (choices as buttons)
@@ -35,3 +89,4 @@ var questions = []
 //create time-out function that hides question/answers
 
 //
+console.log(whoC.quest);
